b91fc8eaea5826d0ca8bf6c8be65afb8e27136b5
Yao Li <liyaokok@gmail.com>
2023-08-28 19:51:55 -0400

    Add my employees to nav bar

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -13,4 +13,8 @@ class Employee < ApplicationRecord
     def tasks
         Task.where(id: EmployeeTaskMap.where(employee_id: self.id).pluck(:task_id))
     end
+
+    def manager?
+        self.employee_role.name != "general"
+    end
 end
