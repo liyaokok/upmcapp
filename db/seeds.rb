f74f6967a97711c5ee44b88cfb73c7cc2a7053f1
Yao Li <liyaokok@gmail.com>
2023-08-28 15:07:38 -0400

    seed the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# employee_roles
+EmployeeRole.destroy_all
+
+EmployeeRole.create!([
+    {
+        name: "general"
+    },
+    {
+        name: "department manager"
+    },
+    {
+        name: "company manager"
+    }
+])
+
+# departments
+Department.destroy_all
+
+departments = [Department.create!(name: "emergecy"), Department.create!(name: "internal medicine"), Department.create!(name: "surgery")]
+
+# tasks
+Task.destroy_all
+
+for department in departments do
+    for i in 1..3 do
+        Task.create!(name: department.name + " task " + i.to_s, department: department);
+    end
+end
