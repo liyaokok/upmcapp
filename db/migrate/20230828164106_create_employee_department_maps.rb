d3caa2432515f4b9b3d754b96694bf083ef8fe68
Yao Li <liyaokok@gmail.com>
2023-08-28 19:40:22 -0400

    Update map tables with uniqueness

--- db/migrate/20230828164106_create_employee_department_maps.rb
+++ db/migrate/20230828164106_create_employee_department_maps.rb
@@ -4,6 +4,8 @@ class CreateEmployeeDepartmentMaps < ActiveRecord::Migration[7.0]
       t.belongs_to :department, null: false
       t.belongs_to :employee, null: false
 
+      t.index [:department_id, :employee_id], unique: true
+
       t.timestamps
     end
   end
--- db/migrate/20230828164107_create_employee_task_maps.rb
+++ db/migrate/20230828164107_create_employee_task_maps.rb
@@ -4,6 +4,8 @@ class CreateEmployeeTaskMaps < ActiveRecord::Migration[7.0]
       t.belongs_to :task, null: false
       t.belongs_to :employee, null: false
 
+      t.index [:task_id, :employee_id], unique: true
+
       t.timestamps
     end
   end
