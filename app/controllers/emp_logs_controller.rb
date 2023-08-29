dd13926fe817f13ada1c18afcbea5256e0871606
Yao Li <liyaokok@gmail.com>
2023-08-29 19:20:34 -0400

    update emp log page

--- app/controllers/emp_logs_controller.rb
+++ app/controllers/emp_logs_controller.rb
@@ -2,5 +2,12 @@ class EmpLogsController < ApplicationController
     before_action :require_company_manager_role!
 
     def new
+        @entries = recent_entries
+    end
+
+    private
+
+    def recent_entries
+        EmpLog.order("created_at DESC").limit(20)
     end
 end
