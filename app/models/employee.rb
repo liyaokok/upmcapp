50e6989beec8fec8d9a8579020a81c5880ae94d0
Yao Li <liyaokok@gmail.com>
2023-08-29 18:49:54 -0400

    Add email validation

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -4,7 +4,7 @@ class Employee < ApplicationRecord
     belongs_to :employee_role
 
     validates :name, presence: true, allow_blank: false
-    validates :email, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
 
     def departments
         Department.where(id: EmployeeDepartmentMap.where(employee_id: self.id).pluck(:department_id))
