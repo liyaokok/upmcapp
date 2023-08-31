ed653f9cc01e4b1d201af1929c0a63bb51c2cb62
Yao Li <liyaokok@gmail.com>
2023-08-31 10:17:09 -0400

    employee salary is non negative number

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -5,6 +5,7 @@ class Employee < ApplicationRecord
 
     validates :name, presence: true, allow_blank: false
     validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
+    validates :salary, numericality: { greater_than_or_equal_to: 0 }
 
     def departments
         Department.where(id: EmployeeDepartmentMap.where(employee_id: self.id).pluck(:department_id))
